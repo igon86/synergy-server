d3116355206b59a7762b02e275e4a66e0c5113a1
andre.lotta86 <andre.lotta86@38894c1f-1372-cd39-e9b7-62cbe97829a7>
2012-01-19 13:24:25 +0000

    package netbeans

---
+++ SynergyServer/src/client/SynergyClient.java
@@ -0,0 +1,82 @@
+
+package client;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author andrealottarini
+ */
+public class SynergyClient {
+
+    private static final int port = 24800;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnknownHostException {
+
+	if(args.length != 1 ) {
+			System.out.println("Incorrect Number of parameters");
+			System.exit(-1);
+	}
+
+        InetAddress localhost = java.net.InetAddress.getLocalHost();
+        String local = localhost.getCanonicalHostName();
+        System.out.println("This hostanme is: "+local);
+
+        Socket s = new Socket();
+        System.out.println("Socket creata");
+
+        InetAddress server = InetAddress.getByName(args[1]);
+
+        SocketAddress sockaddr = new InetSocketAddress(server, port);
+        try {
+            //timeout in millisecondi
+            s.connect(sockaddr, 2000);
+
+        } catch (IOException ex) {
+            System.out.println("Server down: muoro");
+            System.exit(-1);
+        }
+        System.out.println("Connesso");
+
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(s.getOutputStream());
+        } catch (IOException ex) {
+            System.out.println("Unable to open OOstream");
+            System.exit(1);
+        }
+
+        try {
+            out.writeUTF(local);
+        } catch (IOException ex) {
+            System.out.println("Unable to write on the stream");
+            System.exit(1);
+        }
+
+        try {
+            out.close();
+        } catch (IOException ex) {
+            System.out.println("Unable to close OOstream");
+            System.exit(-1);
+        }
+
+        try {
+            s.close();
+        } catch (IOException ex) {
+            System.out.println("Unable to close Socket");
+            System.exit(-1);
+        }
+    }
+
+
+}
+
---
+++ SynergyServer/src/server/SynergyServer.java
@@ -0,0 +1,77 @@
+
+
+package server;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ *
+ * @author andrealottarini
+ */
+public class SynergyServer {
+
+  private static final int port = 24800;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        ObjectInputStream in = null;
+
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: " + port);
+            System.exit(-1);
+        }
+
+
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.out.println("Accept failed: " + port);
+            System.exit(-1);
+        }
+
+        try {
+            in = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+        }
+
+        String ridden = "";
+        try {
+            ridden = in.readUTF();
+        } catch (IOException ex) {
+            System.out.println("Unable to read from Stream");
+        }
+
+        System.out.println(ridden);
+
+        try {
+            in.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            System.out.println("Unable to close socket or stream");
+        }
+
+        // scrivo l'hostname del tipo su file
+        try {
+            // Create file
+            FileWriter fstream = new FileWriter("out.txt");
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(ridden);
+            //Close the output stream
+            out.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
+
