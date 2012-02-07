b5d4a8094801739b2f62f7afa84b47fe56e92ca0
andre.lotta86 <andre.lotta86@38894c1f-1372-cd39-e9b7-62cbe97829a7>
2012-02-07 11:22:08 +0000

    forse cosi va

--- trunk/SynergyServer/src/server/SynergyServer.java
+++ trunk/SynergyServer/src/server/SynergyServer.java
@@ -75,6 +75,7 @@ public class SynergyServer {
             
             //Close the output stream
             out.close();
+            out2.close();
         } catch (Exception e) {//Catch exception if any
             System.err.println("Error: " + e.getMessage());
         }
